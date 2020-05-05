4515a4a0a7b84d5e2a7692d62920c9cf435a04d2
Kate Mills <kate.mills1124@gmail.com>
2020-05-04 17:37:14 -0700

    Formatting

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,19 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
 import "./bootstrap.min.css"
 import "./layout.css"
 
 import Navbar from "./Globals/Navbar"
 
-
 const Layout = ({ children }) => {
   return (
     <>
-      <Navbar />  
+      <Navbar />
       {children}
     </>
   )
 }
-
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
