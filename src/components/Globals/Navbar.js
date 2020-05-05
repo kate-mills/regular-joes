062468c417eba0927074b7aace5aa21bf09352c8
Kate Mills <kate.mills1124@gmail.com>
2020-05-04 17:36:37 -0700

    Background Section

--- src/components/Globals/Navbar.js
+++ src/components/Globals/Navbar.js
@@ -3,10 +3,6 @@ import {Link} from "gatsby"
 import logo from "../../images/logo.svg"
 import {FaCartArrowDown} from "react-icons/fa"
 
-
-
-
-
 export default class Navbar extends Component {
   state = {
     navbarOpen: false,
@@ -26,7 +22,6 @@ export default class Navbar extends Component {
   };
 
   navbarHandler = () => {
-    console.log('hello');
     this.state.navbarOpen? this.setState({navbarOpen: false, css:'collapse navbar-collapse'}):
       this.setState({navbarOpen: true, css:'collapse navbar-collapse show'})
   }; 
@@ -36,7 +31,7 @@ export default class Navbar extends Component {
       <nav className="navbar navbar-expand-sm
       bg-light navbar-light">
         <Link to="/" className="navbar-brand">
-          <img src={logo}/>
+          <img alt="" src={logo}/>
           {/* https://www.iconfinder.com/icons/185113/coffee_streamline_icon Creative Commons (Attribution 3.0 Unported); https://www.iconfinder.com/webalys */}
         </Link>
         <button className="navbar-toggler" type="button" onClick={this.navbarHandler}>
