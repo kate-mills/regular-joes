eab83743ffdabc8ea5a86968af6b9d8242580910
Kate Mills <kate.mills1124@gmail.com>
2020-05-09 12:13:44 -0700

    fix(snipcart) data-item-url changed to 'https://hardcore-coffeenetlify.app/'

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -15,7 +15,7 @@ export default function Product({ product }) {
             data-item-name={product.title}
             data-item-price={product.price}
             data-item-image={product.image.fluid.src}
-            data-item-url="/"
+            data-item-url="https://hardcore-coffee.netlify.app/"
           >
             add to cart
           </button>
